bc8062c521073f66b20a48d2597e00e8a8c13da8
Rishi Shah <110341315+21ce131@users.noreply.github.com>
2022-09-29 20:40:04 +0530

    Create part6_1.java

---
+++ part6_1.java
@@ -0,0 +1,29 @@
+/*NAME : RISHI SHAH
+ID : 21CE131
+AIM : Write a program to create thread which display “Hello World” message.
+A. by extending Thread class
+B. by using Runnable interface*/
+
+class ThreadDemo extends Thread {
+
+    public void run() {
+        System.out.println("Hello World from Thread class");
+    }
+}
+
+class RunnableDemo implements Runnable {
+
+    public void run() {
+        System.out.println("Hello World from Runnable Interface");
+    }
+}
+
+public class pra_6_1{
+    public static void main(String[] args) {
+        ThreadDemo td = new ThreadDemo();
+        Thread rd = new Thread(new RunnableDemo());
+        td.start();
+        rd.start();
+    }
+
+}
